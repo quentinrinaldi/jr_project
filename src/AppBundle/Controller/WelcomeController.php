53e432a899b04930eca14622621ba33607c6b64f
quentinrinaldi <quentinrinaldi@gmail.com>
2016-02-03 13:55:06 +0100

    Ebauche de la page d'accueil

--- src/AppBundle/Controller/WelcomeController.php
+++ src/AppBundle/Controller/WelcomeController.php
@@ -5,11 +5,12 @@
 namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class WelcomeController
+class WelcomeController extends Controller
 {
     public function welcomeAction()
     {
-        return new Response("Hello World !");
+        return $this->render('index.html.twig',array());
     }
 }
